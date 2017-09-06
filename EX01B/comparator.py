0ade804e81ae4ef8abbd79403a2d26095bcba2c8
mavuks <mavuks@ttu.ee>
2017-09-06 14:30:46 +0300

    muutsin stiili

---
+++ EX01B/comparator.py
@@ -0,0 +1,16 @@
+def comparator(num: float) -> str:
+    """
+    Compare the given number with 5 and return message depending on the number's value.
+
+    :param num: input number
+    :return: message regarding the number value compared to five.
+    """
+    # Your code here
+    if num > 5:
+        return "The input number is bigger than 5!"
+    elif num < 5:
+        return "The input number is smaller than 5!"
+    else:
+        return "The input number is 5!"
+
+print (comparator (6))
