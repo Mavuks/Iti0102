f6f314fbb17ff58bfb867e21a753c1056971fe65
mavuks <mavuks@ttu.ee>
2017-09-08 13:38:05 +0300

    uus

---
+++ EX02A/calc.py
@@ -0,0 +1,39 @@
+import math
+
+"""Calculate the value of z."""
+
+
+def value_of_z(ex, x, y):
+    """
+    Calculate the value of z with given x and y.
+
+    :param ex: exercise number
+    :param x: 4
+    :param y: 4
+    :return: float
+    """
+
+    # Your code here
+
+
+    if ex == 1:
+        return (x ** y + y ** x)
+
+    elif ex is 2:
+        return ((x/5.6)-(y/6.5))
+    elif ex is 3:
+        return ((x/5)*y**4*(math.log(5))/(7* math.sqrt(x**2+y**2)+1))
+    else:
+        print ("sellist ülesannet ei ole!")
+
+
+
+
+
+# testid
+print(value_of_z(1, 0, 0))  # -> 2
+print(value_of_z(2, 50, 1))  # -> 8.774725274725276
+print(value_of_z(3, 99, 99))  # -> 3120247.5768747954
+print(value_of_z(4, 0, 0))  # None
+
+
