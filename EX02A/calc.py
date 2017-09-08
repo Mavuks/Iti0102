5d27b78589cee81087542f00f5a3dab05d95e601
mavuks <mavuks@ttu.ee>
2017-09-08 16:42:46 +0300

    uus

--- EX02A/calc.py
+++ EX02A/calc.py
@@ -13,13 +13,13 @@ def value_of_z(ex, x, y):
     """
     # Your code here
     if ex is 1:
-            return(x ** y + y ** x)
+            return (x ** y + y ** x)
     elif ex is 2:
-            return((x / 5.6) - (y / 6.5))
+            return ((x / 5.6) - (y / 6.5))
     elif ex is 3:
-            return((x / 5) * y ** 4 * (math.log(5)) / (7 * math.sqrt(x ** 2 + y ** 2) + 1))
+            return ((x / 5) * y ** 4 * (math.log(5)) / (7 * math.sqrt(x ** 2 + y ** 2) + 1))
     else:
-            return "sellist ülesannet ei ole!"
+            print ("sellist ülesannet ei ole!")
 # testid
 
 
