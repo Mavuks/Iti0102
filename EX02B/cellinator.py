01950dfd20b52898accbfbaaabb9a83bfb1fc224
mavuks <mavuks@ttu.ee>
2017-09-17 15:34:01 +0300

    uus

--- EX02B/cellinator.py
+++ EX02B/cellinator.py
@@ -50,7 +50,7 @@ def get_row_len(row, col, cell_index):
     """
     row_len = (cell_index - col) / row
 
-    if row  == 0 or ((cell_index - row) / col) < row:
+    if row == 0 or ((cell_index - col) / row) < col:
         return -1
     else:
         return row_len
@@ -76,3 +76,4 @@ if __name__ == '__main__':
     print(get_row_len(1, 0, 3))  # => 3
     print(get_row_len(12, 0, 12))  # => 1
     print(get_row_len(0, 0, 0))
+    print(get_row_len(0, 3, 18))
