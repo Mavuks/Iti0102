56c577b09ed9aa18e3b73ab6c2bcf5c36567c718
mavuks <mavuks@ttu.ee>
2017-09-24 20:34:12 +0300

    uus

--- EX03B/timekeeping.py
+++ EX03B/timekeeping.py
@@ -1,30 +1,68 @@
+''' Timekeeping.'''
+
+
 def seconds(time_string, from_seconds_in_minute):
+    '''
+
+    :param time_string: current time as a time_string
+    :param from_seconds_in_minute: current amount of seconds in one minute
+    :return:amount seconds
+    '''
     a = from_seconds_in_minute
     m, s = time_string.split(':')
     return (int(m) * a + int(s))
 
 
-
 def minute_count(time_string, from_seconds_in_minute, to_seconds_in_minute):
+    '''
+
+    :param time_string: current time as a time_string
+    :param from_seconds_in_minute: current amount seconds in one minute
+    :param to_seconds_in_minute: new amount seconds in one minute
+    :return: new amount minute in new system
+    '''
     new = to_seconds_in_minute
     z = seconds(time_string, from_seconds_in_minute)
     k = z // new
     return k
 
+
 def seconds_count(time_string, from_seconds_in_minute, to_seconds_in_minute):
+    '''
+
+    :param time_string: current time as a time_string
+    :param from_seconds_in_minute: current amount seconds in one minute
+    :param to_seconds_in_minute: new amount seconds in one minute
+    :return:new amount seconds in new system
+    '''
     new = to_seconds_in_minute
     z = seconds(time_string, from_seconds_in_minute)
     h = z % new
     return h
 
+
 def final(time_string, from_seconds_in_minute, to_seconds_in_minute):
+    '''
+
+    :param time_string: current time as a time_string
+    :param from_seconds_in_minute: current amount seconds in one minute
+    :param to_seconds_in_minute: new amount seconds in one minute
+    :return: new time_string
+    '''
     k = minute_count(time_string, from_seconds_in_minute, to_seconds_in_minute)
     h = seconds_count(time_string, from_seconds_in_minute, to_seconds_in_minute)
     return f'{str(k).zfill(2) }:{str(h).zfill(2)}'
 
 
-
 def convert(time_string, from_seconds_in_minute, to_seconds_in_minute):
+    '''
+    Convert time from one system to another.
+
+    :param time_string: current time as a time_string
+    :param from_seconds_in_minute: current amount seconds in one minute
+    :param to_seconds_in_minute: new amount seconds in one minute
+    :return: converted time_String
+    '''
     a = from_seconds_in_minute
     m, s = time_string.split(':')
     if str(s) >= str (a):
@@ -34,7 +72,8 @@ def convert(time_string, from_seconds_in_minute, to_seconds_in_minute):
     e = seconds_count(time_string, from_seconds_in_minute, to_seconds_in_minute)
     u = str(q).zfill(2)
     o = str(e).zfill(2)
-    return f'{u}:{o}'
+    result = f'{u}:{o}'
+    return result
 
 
 
