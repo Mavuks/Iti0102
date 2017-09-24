8421ee2ddb0b4e8d5de31b6a9f0d2bb30317b3ce
mavuks <mavuks@ttu.ee>
2017-09-24 16:54:01 +0300

    uus

--- EX03B/timekeeping.py
+++ EX03B/timekeeping.py
@@ -1,28 +1,47 @@
-def seconds(time_string, from_seconds_in_minute, to_seconds_in_minute):
+def time(time_string):
+    return time(time_string)
+
+def seconds(time_string, from_seconds_in_minute):
     a = from_seconds_in_minute
     m, s = time_string.split(':')
-    if int(s) > int(a):
-        return "None"
-    else:
-        return(int(m) * a + int(s))
+    return (int(m) * a + int(s))
+
 
 
 def minute_count(time_string, from_seconds_in_minute, to_seconds_in_minute):
     new = to_seconds_in_minute
-    z = seconds(time_string, from_seconds_in_minute, to_seconds_in_minute)
+    z = convert(time_string, from_seconds_in_minute, to_seconds_in_minute)
     k = z // new
     return k
 
 def seconds_count(time_string, from_seconds_in_minute, to_seconds_in_minute):
     new = to_seconds_in_minute
-    z = seconds(time_string, from_seconds_in_minute, to_seconds_in_minute)
+    z = convert(time_string, from_seconds_in_minute, to_seconds_in_minute)
     h = z % new
     return h
 
-def convert(time_string, from_seconds_in_minute, to_seconds_in_minute):
+def final(time_string, from_seconds_in_minute, to_seconds_in_minute):
     k = minute_count(time_string, from_seconds_in_minute, to_seconds_in_minute)
     h = seconds_count(time_string, from_seconds_in_minute, to_seconds_in_minute)
     return f'{str(k).zfill(2) }:{str(h).zfill(2)}'
+
+
+
+def convert(time_string, from_seconds_in_minute, to_seconds_in_minute):
+
+    minutes, seconds = time(time_string)
+
+
+
+    seconds_to_convert = minutes * from_seconds_in_minute + seconds
+    print(seconds_to_convert)
+    result = final(seconds_to_convert, to_seconds_in_minute)
+    return result
+
+
+
+
+
 
 
 print(convert('12:12', 20, 30))
