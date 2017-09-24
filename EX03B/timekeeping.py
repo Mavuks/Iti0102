6fb888593c0840b2db01d6fd2c9a9e2e2b9176c5
mavuks <mavuks@ttu.ee>
2017-09-24 15:49:17 +0300

    uus

--- EX03B/timekeeping.py
+++ EX03B/timekeeping.py
@@ -1,4 +1,4 @@
-def convert(time_string, from_seconds_in_minute, to_seconds_in_minute):
+def seconds(time_string, from_seconds_in_minute, to_seconds_in_minute):
     a = from_seconds_in_minute
     m, s = time_string.split(':')
     if int(s) > int(a):
@@ -9,23 +9,22 @@ def convert(time_string, from_seconds_in_minute, to_seconds_in_minute):
 
 def minute_count(time_string, from_seconds_in_minute, to_seconds_in_minute):
     new = to_seconds_in_minute
-    z = convert(time_string, from_seconds_in_minute, to_seconds_in_minute)
+    z = seconds(time_string, from_seconds_in_minute, to_seconds_in_minute)
     k = z // new
     return k
 
 def seconds_count(time_string, from_seconds_in_minute, to_seconds_in_minute):
     new = to_seconds_in_minute
-    z = convert(time_string, from_seconds_in_minute, to_seconds_in_minute)
+    z = seconds(time_string, from_seconds_in_minute, to_seconds_in_minute)
     h = z % new
     return h
 
-def final(time_string, from_seconds_in_minute, to_seconds_in_minute):
+def convert(time_string, from_seconds_in_minute, to_seconds_in_minute):
     k = minute_count(time_string, from_seconds_in_minute, to_seconds_in_minute)
     h = seconds_count(time_string, from_seconds_in_minute, to_seconds_in_minute)
-    return f'{str(k).zfill(2) }:{str(h).zfill(2) }'
+    return f'{str(k).zfill(2) }:{str(h).zfill(2)}'
 
-'''
-print(final('12:12', 20, 30))
-print(final('03:40', 60, 3))
+
+print(convert('12:12', 20, 30))
+print(convert('03:40', 60, 3))
 print(convert('03:61', 60, 3))
-'''
