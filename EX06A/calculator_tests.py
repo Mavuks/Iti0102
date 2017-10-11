ad055210dcefa90504cdc2312ad6d7f623a3808f
mavuks <mavuks@ttu.ee>
2017-10-11 18:19:25 +0300

    uus

---
+++ EX06A/calculator_tests.py
@@ -0,0 +1,10 @@
+import pytest
+import calculator
+
+def test_repeat_zero_length():
+    # TODO: assert here something
+    pass
+
+def test_repeat_negative_length():
+    # TODO: assert here something
+    pass
