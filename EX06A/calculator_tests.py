0bba0fc22014b23c3562453c3f32ac366265b640
mavuks <mavuks@ttu.ee>
2017-10-12 18:00:33 +0300

    uus

--- EX06A/calculator_tests.py
+++ EX06A/calculator_tests.py
@@ -8,5 +8,27 @@ def test_repeat_negative_length():
     # TODO: assert here something
     pass
 def test_convert_name_savisaar():
-    """Test converting name Savisaar"""
+    """Test converting name Savisaar."""
     assert calculator.convert_name('Savisaar') == 'SAV-8ar'
+
+
+def test_convert_name_empty():
+    """Test empty name."""
+    assert calculator.convert_name('') == 'ERROR'
+
+
+def test_convert_name_no_longer_in_end():
+    """Test returned name to have a lower suffix in the end."""
+    assert calculator.convert_name('TIIGER') == 'TII-6er'
+
+
+def test_convert_name_two_digitas_str_len():
+    """test a name with longer lenght then one digit."""
+    assert calculator.convert_name('PYTHON') == 'PYT-6on'
+
+
+def test_convert_name_error_with_len3():
+    """test name with lenght of 3."""
+    assert calculator.convert_name("ASD") == 'ASD-3sd'
+
+
