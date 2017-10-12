3175d613736b780a87097a12d4ed3a3020585222
mavuks <mavuks@ttu.ee>
2017-10-12 20:36:28 +0300

    uus

--- EX06A/calculator_tests.py
+++ EX06A/calculator_tests.py
@@ -104,6 +104,6 @@ def test_line_not_decorated():
 
 def test_line_decorated():
     """Test decorated."""
-    assert calculator.line(6, decorated=True) == '>------<'
+    assert calculator.line(6, decorated=True) == '>----<'
 
 
