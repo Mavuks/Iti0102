5873b88b3dd0cacdedf3528c0cebda897cda9bc8
mavuks <mavuks@ttu.ee>
2017-10-12 18:52:37 +0300

    uus

--- EX06A/calculator_tests.py
+++ EX06A/calculator_tests.py
@@ -64,5 +64,5 @@ def test_convert_name_subtraction_positive_numbers_fail():
 
 def test_calculator_subtraction_negative_numbers_fail():
     """test subtraction negative nubmers."""
-    assert calculator.subtraction(-4, -3) == '-4 - -3  = -7'
+    assert calculator.subtraction(-4, -3) == '-4 - -3 = -7'
 
