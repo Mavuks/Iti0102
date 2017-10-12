934905a2b4b99daaec09af27ec1fe559375bee89
mavuks <mavuks@ttu.ee>
2017-10-12 21:10:45 +0300

    uus

--- EX06A/calculator_tests.py
+++ EX06A/calculator_tests.py
@@ -109,7 +109,7 @@ def test_line_decorated():
 
 def test_line_decorated_small():
     """Test line decorated small"""
-    assert calculator.line(2, decorated=True) == '><'
+    assert calculator.line(3, decorated=True) == '>-<'
 
 def test_line_not_decorated_small():
     """Test line not decorated small."""
