907af6923f0e3dbe5ac610f7c3171163d34c7cda
mavuks <mavuks@ttu.ee>
2017-10-16 14:54:08 +0300

    uus

--- EX06B/newton_tests.py
+++ EX06B/newton_tests.py
@@ -52,7 +52,7 @@ def test_newton_with_small_numbers():
 
 def test_newton_number_and_iterations_related_1():
     """Number and iterations related 1."""
-    assert newton.square_root_with_newton_method(1.00, 1.9) == 1.25
+    assert newton.square_root_with_newton_method(0.0010, 1) == 1.002
 
 
 def test_newton_number_related_2():
@@ -68,6 +68,9 @@ def test_newton_number_related_1():
 def test_newton_limit_iterations():
     """Limit Iterations."""
     assert newton.square_root_with_newton_method(1500, 10000000) == 38.73
+
+
+
 
 
 
