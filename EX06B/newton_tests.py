69ef8c21b350d992a52bfd91c43e40d52a142c36
mavuks <mavuks@ttu.ee>
2017-10-16 13:51:23 +0300

    uus

--- EX06B/newton_tests.py
+++ EX06B/newton_tests.py
@@ -1,4 +1,15 @@
 import newton
 
 def test_give_it_a_better_name_pls_and_write_a_docstring_pls():
+    """Test."""
     assert newton.square_root_with_newton_method(4, 1) == 2
+
+
+def test_newton_form_zero():
+    """newton zero."""
+    assert newton.square_root_with_newton_method(0, 1) == None
+
+
+def test_newton_iterations_zero():
+    """Number zero."""
+    assert newton.square_root_with_newton_method(4, 0) == None
