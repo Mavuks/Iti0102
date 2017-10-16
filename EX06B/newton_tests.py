6ed16fed8fcfe8941edd413eb51e84695ac7bbdb
mavuks <mavuks@ttu.ee>
2017-10-16 16:37:44 +0300

    uus

--- EX06B/newton_tests.py
+++ EX06B/newton_tests.py
@@ -11,12 +11,12 @@ def test_give_it_a_better_name_pls_and_write_a_docstring_pls():
 
 def test_newton_form_zero():
     """newton zero."""
-    assert None in newton.square_root_with_newton_method(0, 1)
+    assert newton.square_root_with_newton_method(0, 1) == None
 
 
 def test_newton_iteration_zero():
     """iteration zero."""
-    assert None in newton.square_root_with_newton_method(4, -1)
+    assert newton.square_root_with_newton_method(4, -1) == None
 
 
 def test_newton_rounding_2():
