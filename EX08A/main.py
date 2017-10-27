f1633bae97a61b2938b4af33ae48999a7b1a096b
mavuks <mavuks@ttu.ee>
2017-10-27 21:50:58 +0300

    uus

--- EX08A/main.py
+++ EX08A/main.py
@@ -23,14 +23,14 @@ if __name__ == '__main__':
     print(get_reversed_acronym("Light amplification by the stimulated emission of radiation."))  # --> RESAL
     print(get_reversed_acronym("Self-contained underwater breathing apparatus."))  # --> ABUS
     print(get_reversed_acronym("   Spaces     are    irrelevant   "))  # --> IS
-
     mes = \
-    """
-    As soon as the light in the bedroom went out there was a stirring and a
-    fluttering all through the farm buildings. Word had gone round during the
-    day that old Major, the prize Middle White boar, had had a strange dream
-    on the previous night and wished to communicate it to the other animals.
-    It had been agreed that they should all meet in the big barn as soon as
-    Mr. Jones was safely out of the way.
-    """
+        """
+        As soon as the light in the bedroom went out there was a stirring and a
+        fluttering all through the farm buildings. Word had gone round during the
+        day that old Major, the prize Middle White boar, had had a strange dream
+        on the previous night and wished to communicate it to the other animals.
+        It had been agreed that they should all meet in the big barn as soon as
+        Mr. Jones was safely out of the way.
+        """
+
     print(get_reversed_acronym(mes))  # --> Sorry, the input's just too long!
