a224ac03badfdd3f37d47ee1314202a28d91c961
mavuks <mavuks@ttu.ee>
2017-11-15 22:35:49 +0200

    UUS!

--- EX11A/bank_account.py
+++ EX11A/bank_account.py
@@ -4,11 +4,11 @@
 class BankAccount:
     """BankAccount."""
 
-
     def __init__(self, name, balance):
         """A."""
         self.name = name
         self.balance = balance
+
 
     def withdraw(self, amount):
         """A."""
@@ -19,6 +19,7 @@ class BankAccount:
         self.balance -= amount
         return True
 
+
     def deposit(self, amount):
         """A."""
         if amount > 0:
@@ -27,13 +28,15 @@ class BankAccount:
             pass
 
 
-
     def get_balance(self):
         """A."""
         return self.balance
+
+
     def get_name(self):
         """A."""
         return self.name
+
 
     def transfer(self, target, amount, fee):
         """A."""
@@ -58,14 +61,14 @@ if __name__ == '__main__':
     assert abs(mary_account.get_balance() - 110) < 0.001
     mary_account.deposit(-10)
     assert abs(mary_account.get_balance() - 110) < 0.001
-    assert mary_account.withdraw(-10) == False
+    assert mary_account.withdraw(-10) is False
     assert abs(mary_account.get_balance() - 110) < 0.001
-    assert mary_account.withdraw(1000) == False
+    assert mary_account.withdraw(1000) is False
     assert abs(mary_account.get_balance() - 110) < 0.001
-    assert mary_account.withdraw(10) == True
+    assert mary_account.withdraw(10) is True
     assert abs(mary_account.get_balance() - 100) < 0.001
 
-    assert mary_account.transfer(guido_account, 10) == True
+    assert mary_account.transfer(guido_account, 10) is True
     assert abs(mary_account.get_balance() - 89.9) < 0.001
     assert abs(guido_account.get_balance() - 160) < 0.001
 
@@ -73,6 +76,21 @@ if __name__ == '__main__':
     print("Add asserts for transferring between the same account")
     # TODO: Add asserts for transferring from mary_account to mary_second_account
     # and from mary_account to mary_account
+    # asserts for transferring between the same account
+    assert mary_account.transfer(mary_account, 10, 1000) is True
+    assert abs(mary_account.get_balance() - 89.9 < 0.001)
+    # asserts for transferring between the accounts of the same owner
+    assert mary_account.transfer(mary_second_account, 10, 10) is True
+    assert abs(mary_account.get_balance() - 29.9 < 0.001)
+    assert abs(mary_second_account.get_balance() - 88.01 < 0.001)
+    # test_transfer_to_non_existing_account
+    mary_acc = 0
+    assert mary_account.transfer(mary_acc, 10, 10) is False
+    # test transfer neg amount
+    assert mary_account.transfer(mary_account, -10, 10) is False
+    # test transfer 0 and neg fee
+    assert mary_account.transfer(guido_account, 0, -1) is True
+    print("And now you are ready for submission!")
     assert False
 
     print("And now you are ready for submission!")
