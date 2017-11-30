5be4530dd01a8a4dd36f4dba81466b1031f5560d
mavuks <mavuks@ttu.ee>
2017-11-30 18:19:34 +0200

    UUS!

---
+++ EX13B/phone.py
@@ -0,0 +1,19 @@
+def how_many_calls(n):
+    """
+    Return the number of calls made during the current minute.
+
+    Arguments:
+    n -- the current minute.
+    """
+    if n < 0:
+        return None
+
+def how_many_people(n):
+    """
+    Return the number of people who know after n minutes has passed.
+
+    Arguments:
+    n -- how many minutes has passed.
+    """
+    if n < 0:
+        return None
