5aed3a7e5b1641a8886e7222e8adf16218984edf
mavuks <mavuks@ttu.ee>
2017-11-30 20:46:17 +0200

    UUS!

--- EX13B/phone.py
+++ EX13B/phone.py
@@ -22,7 +22,7 @@ def how_many_calls(n):
     elif n == 3:
         return 4
     elif n > 3:
-        return how_many_calls(n - 1) + how_many_calls(n - 2) + how_many_calls(n - 3)
+        return int (how_many_calls(n - 1) + how_many_calls(n - 2) + how_many_calls(n - 3))
 
 @lru_cache(maxsize = 1000)
 def how_many_people(n):
@@ -32,16 +32,16 @@ def how_many_people(n):
     Arguments:
     n -- how many minutes has passed.
     """
-    # if type(n) != int:
-    #     return None
-    # elif n < 0:
-    #     return None
-    # elif n == 0:
-    #     return 1
-    # elif n == 1:
-    #     return 2
-    # elif n > 2:
-    #      return how_many_people(n) + how_many_calls(n)
+    if type(n) != int:
+        return None
+    elif n < 0:
+        return None
+    elif n == 0:
+        return 1
+    elif n == 1:
+        return 2
+    elif n > 1:
+         return how_many_people(n - 1) + how_many_calls(n)
 
 
 
@@ -52,6 +52,6 @@ if __name__ == "__main__":
     print(how_many_calls(2))  # -> 2
     print(how_many_calls(4))  # -> 7
     print(how_many_calls(10))  # -> 274
-    print(how_many_people(2))  # -> 4
+    print(how_many_people(1))  # -> 4
     print(how_many_people(4))  # -> 15
     print(how_many_people(10))  # -> 600
