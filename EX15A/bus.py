0cc0e26e5986f8249b89d9ba9dc82504fb8b1775
mavuks <mavuks@ttu.ee>
2017-12-17 17:44:54 +0200

    UUS!

--- EX15A/bus.py
+++ EX15A/bus.py
@@ -3,8 +3,9 @@
 import datetime
 
 
-class input:
+class Input:
     """Tere."""
+
     timeformat = "%H:%M"
     time = input("What's your time?")
     try:
@@ -13,8 +14,9 @@ class input:
         print("Use HH:SS format!")
 
 
-class File:
-    """Loeb faili."""
+# class File:
+#     """Loeb faili."""
+#     with open("bussiajad.txt") as f:
 
 
 class Main:
@@ -22,3 +24,18 @@ class Main:
 
     def __init__(self, file: str):
         """Tere."""
+        self.info_dict = {}
+        with open(file, 'r') as f:
+            self.file_lines = f.readlines()
+
+
+    def dict(self):
+        """Dict."""
+        for line in self.file_lines:
+            new_list = line.split('\t')
+            self.info_dict[new_list[0]] = new_list[1:]
+        return self.info_dict
+
+    def get_departure_time(self):
+        """Tere."""
+
