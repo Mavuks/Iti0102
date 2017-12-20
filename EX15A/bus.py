04c06f31fbda837180cbb4f84c46d0608c91fa86
mavuks <mavuks@ttu.ee>
2017-12-20 13:48:08 +0200

    UUS!

--- EX15A/bus.py
+++ EX15A/bus.py
@@ -1,41 +1,58 @@
-"""Bussiajad."""
-
-import datetime
-
-
-class Input:
-    """Tere."""
-
-    timeformat = "%H:%M"
-    time = input("What's your time?")
-    try:
-        validtime = datetime.datetime.strptime(time, timeformat)
-    except ValueError:
-        print("Use HH:SS format!")
-
-
-# class File:
-#     """Loeb faili."""
-#     with open("bussiajad.txt") as f:
+"""When will my bus leave?."""
 
 
 class Main:
-    """Main."""
+    """Main class."""
 
     def __init__(self, file: str):
-        """Tere."""
-        self.info_dict = {}
-        with open(file, 'r') as f:
-            self.file_lines = f.readlines()
+        """initi."""
 
+        self.given_minutes = 0
+        self.closest_time = 0
+        self.departing_minutes = self.format_text(file)
 
-    def dict(self):
-        """Dict."""
-        for line in self.file_lines:
-            new_list = line.split('\t')
-            self.info_dict[new_list[0]] = new_list[1:]
-        return self.info_dict
+    def format_text(self, file: str):
+        """Format text."""
+
+        departing_minutes = []
+        with open(file) as text:
+            for line in text:
+                split_data = line.split()
+                for minute in split_data[1:]:
+                    departing_minutes.append(int(split_data[0]) * 60 + int(minute))
+        return departing_minutes
+
+    def get_closest_time(self):
+        """Get closest time."""
+        departing = ""
+        for leaving in self.departing_minutes:
+            if leaving > self.given_minutes % max(self.departing_minutes):
+                departing = leaving
+                break
+        return departing
 
     def get_departure_time(self):
-        """Tere."""
+        """Get depature time."""
+        given_time = self.ask_user_time()
+        self.given_minutes = given_time[0] * 60 + given_time[1]
+        self.closest_time = self.get_closest_time()
+        hours = self.closest_time // 60
+        minutes = str(self.closest_time % 60).zfill(2)
+        print("Your bus will depart at {HH}:{mm}".format(HH=hours, mm=minutes))
+        return "Your bus will depart at {HH}:{mm}".format(HH=hours, mm=minutes)
 
+    def ask_user_time(self):
+        """User time."""
+
+        given_time = input()
+        if ":" in given_time:
+            given_time = given_time.split(":")
+            if given_time[0].isnumeric() and given_time[1].isnumeric():
+                if 1 <= len(given_time[0]) <= 2 and len(given_time[1]) == 2:
+                    if 0 <= int(given_time[0]) < 25 and 0 <= int(given_time[1]) <= 59:
+                        return int(given_time[0]), int(given_time[1])
+        raise Exception
+
+
+
+Main("bussiajad.txt").get_departure_time()
